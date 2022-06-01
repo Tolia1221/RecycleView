8338c6290344c1cb8126dc97be57dc574e05edbb
AnatoliiBalikov <a.balikov.dunice.@gmail.com>
2022-06-01 21:00:26 +0300

    second

--- app/src/main/java/com/example/plantshandbook/MainActivity.kt
+++ app/src/main/java/com/example/plantshandbook/MainActivity.kt
@@ -2,11 +2,34 @@ package com.example.plantshandbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.HorizontalScrollView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.plantshandbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding//подключаем ViewBinding
+    private val adapter=PlantAdapter()
+    private val imageidListt  = listOf (R.drawable.grogi,R.drawable.grogi,R.drawable.grogi)
+    private var index=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val binding=PlantItemBinding.bind(item)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)//надуваем наш холдер
+        setContentView(binding.root)
+        init()
+    }
+    private fun init(){
+        binding.apply {
+            rcView.layoutManager=GridLayoutManager(this@MainActivity,3)
+            rcView.adapter=adapter
+
+            if(index>4) index=0
+            val plant=Plant(imageidListt[index],"$index")
+            index++
+
+
+            adapter.addPlant(plant)
+        }//пишем apply для того чтобы мы могли получить доступ напрямую к элементам
     }
 }
--- app/src/main/java/com/example/plantshandbook/PlantAdapter.kt
+++ app/src/main/java/com/example/plantshandbook/PlantAdapter.kt
@@ -1,19 +1,47 @@
 package com.example.plantshandbook
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.plantshandbook.databinding.ImageLayoutBinding
 
 
-class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantHolder> {
+class PlantAdapter: RecyclerView.Adapter<PlantAdapter.PlantHolder>() {
+    val plantList=ArrayList<Plant>()
     class PlantHolder(item: View): RecyclerView.ViewHolder(item) {
-        val binding = ImageLayoutBinding.bind(item)
-        fun bind(plant:Plant){
-            binding.ivGrogi.setImageResource(plant.idImage)
-
+        val binding = ImageLayoutBinding.bind(item)//  создали переменную которая,наследуется от класса созданного на основе нашего шаблона  Image grogu
+        fun bind(plant:Plant)=with(binding){//приравниваем к байдингу,чтобы каждый раз не вызывать его могло быть
+            // binding.ivGrogi.setImageResource(plant.idImage),станет   ivGrogi.setImageResource(plant.idImage)
+          ivGrogi.setImageResource(plant.idImage)
+tvImage.text=plant.title
 
 
         }
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlantHolder {//этот метод берет нашу разметку Image_layout
+    // надувает ее и заносит в наш класс PlantHolder
+        val view=LayoutInflater.from(parent.context).inflate(R.layout.image_layout,parent,false)
+        return PlantHolder(view)//эта фун-я ждет пока мы передадим ей ViewHolder,передаем ему шаблон для заполнения
+        //Этот класс хранит ссылки на наши элементы View  будет 50 эелеменитов будет 50 ViewHolder
+
+    }
+
+    override fun onBindViewHolder(holder: PlantHolder, position: Int) {//здесь мы начинаем заполнять наш View,который находится в памяти наши TextView,ImageView
+//сюда передается наш класс PlantHolder,чтобы мы из него взяли элементы для заполнения    ivGrogi.setImageResource(plant.idImage)
+//tvImage.text=plant.title
+        holder.bind(plantList[position])
+
+
+    }
+
+    override fun getItemCount(): Int {//передаем размер нашего массива
+return plantList.size
+    }
+    fun addPlant(plant:Plant){
+        plantList.add(plant)
+        notifyDataSetChanged()
     }
 }
